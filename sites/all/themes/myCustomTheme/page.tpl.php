36f3737a69012ff7a304e2aa7f2963a862ea746c
Raj Manchekar <raj.manchekar@lrn.com>
2014-06-24 15:18:40 +0530

    updated

--- sites/all/themes/myCustomTheme/page.tpl.php
+++ sites/all/themes/myCustomTheme/page.tpl.php
@@ -72,17 +72,59 @@
  * @ingroup themeable
  */
 ?>
+<html>
 
-<div class="page-wrapper">
-	<div class="page">
-		<div class="header">
-		<?php print render($page['header']); ?>
-			<div class="navigation-top"><?php if($page['navigation-top']){
-				print render($page['navigation-top']);}; ?></div>
-			<div class="navigation-bottom"><?php if($page['navigation-bottom']){
-				print render($page['navigation-bottom']);}; ?></div>
+<!-- Mirrored from html.realocation.byaviators.com/ by HTTrack Website Copier/3.x [XR&CO'2013], Sat, 15 Mar 2014 10:38:42 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+<link rel="shortcut icon" href="#">
+<link rel="stylesheet" type="text/css" href="libraries/font-awesome/css/font-awesome.css" media="screen, projection">
+<link rel="stylesheet" type="text/css" href="libraries/jquery-bxslider/jquery.bxslider.css" media="screen, projection">
+<link rel="stylesheet" type="text/css" href="libraries/flexslider/flexslider.css" media="screen, projection">
+<link rel="stylesheet" type="text/css" href="css/realocation.css" media="screen, projection" id="css-main">
+<link href="http://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet" type="text/css">
+
+<title>
+        Realocation | Modern Real Estate Template
+</title>
+</head>
+
+<body>
+
+<div id="wrapper">
+		<div id="header-wrapper">
+			<div id="header">
+				<div id="header-inner">
+					<div class="header-bar">
+						<div class="container">
+							<?php if($page['header-bar']){
+							print render($page['header-bar']);}; ?>
+						
+						</div>
+					</div>
+					
+					<div class="header-top">
+						<div class="container">
+							<?php if($page['header-top']){
+							print render($page['header-top']);}; ?>
+						
+						</div>
+					</div>
+					
+					<div class="header-navigation">
+						<div class="container">
+							<?php if($page['header-navigation']){
+							print render($page['header-navigation']);}; ?>
+						
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-		<div class="main-content">
+		<div id="main-wrapper">
 			<div class="left-sidebar"><?php if($page['left-sidebar']){
 					print render($page['left-sidebar']);} ?></div>
 			<div class="content">Main Content<?php if($page['content']){
@@ -94,17 +136,48 @@
 			<div class="static-content"><?php if($page['static-content']){
 					print render($page['static-content']); };?></div>
 		</div>
-		<div class="footer">
-			<div class="footer1"><?php if($page['footer1']){
-				print render($page['footer1']);};?></div>
-			<div class="footer2"><?php if($page['footer2']){
-				print render($page['footer2']);}; ?></div>
-			<div class="footer3"><?php if($page['footer3']){
-				print render($page['footer3']);}; ?></div>
-			<div class="footer-middle"><?php if($page['footer-middle']){
-				print render($page['footer-middle']);}; ?></div>
-			<div class="footer-bottom"><?php if($page['footer-bottom']){
+		
+		<div id="footer-wrapper">
+			<div id="footer">
+				<div id="footer-inner">
+					<div id="footer-top">
+						<div class="container">
+							<div class="row">
+								<div class="widget col-sm-8">
+								<?php if($page['footer-top']){
+								print render($page['footer-top']);}; ?></div>
+								<div class="widget col-sm-4"></div>
+							</div>
+							<div class="row">
+								<?php if($page['footer-middle']){
+								print render($page['footer-middle']);}; ?>
+							</div>
+						</div>
+					</div>
+					<div id="footer-bottom">
+						<div id="container"><?php if($page['footer-bottom']){
 				print render($page['footer-bottom']);}; ?></div>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div> <!-- end of page-wrapper -->
+		
 </div><!-- end of page-wrapper -->
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="libraries/isotope/jquery.isotope.min.js"></script>
+
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?v=3&amp;sensor=true"></script>
+<script type="text/javascript" src="js/gmap3.infobox.js"></script>
+<script type="text/javascript" src="js/gmap3.clusterer.js"></script>
+<script type="text/javascript" src="js/map.js"></script>
+
+<script type="text/javascript" src="libraries/bootstrap-sass/vendor/assets/javascripts/bootstrap/transition.js"></script>
+<script type="text/javascript" src="libraries/bootstrap-sass/vendor/assets/javascripts/bootstrap/collapse.js"></script>
+<script type="text/javascript" src="libraries/jquery-bxslider/jquery.bxslider.min.js"></script>
+<script type="text/javascript" src="libraries/flexslider/jquery.flexslider.js"></script>
+<script type="text/javascript" src="js/jquery.chained.min.js"></script>
+<script type="text/javascript" src="js/realocation.js"></script>
+</body>
+
+<!-- Mirrored from html.realocation.byaviators.com/ by HTTrack Website Copier/3.x [XR&CO'2013], Sat, 15 Mar 2014 10:42:46 GMT -->
+</html>
